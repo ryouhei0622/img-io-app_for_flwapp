9d00355c047a2b2651dcdb72d8b923f474b48f64
t-nakatani <tomoya.12@icloud.com>
2022-06-09 14:36:37 +0900

    add infer_function

--- imageapp/views.py
+++ imageapp/views.py
@@ -6,8 +6,12 @@ from .models import ImageModel
 from django.views.generic import CreateView
 from django.conf import settings
 
-#遊び
-import cv2
+import os, cv2, sys, re, math, Levenshtein, pickle, shutil, math
+import numpy as np
+import pandas as pd
+
+from infer.infer import *
+
 # Create your views here.
 class image_upload(CreateView):
     #テンプレートファイルの連携
@@ -25,15 +29,19 @@ def success(request):
 
 def display_img(request):
 
+
     if request.method == 'GET':
         # getting all the objects of hotel.
         # imgs = img.objects.all()
         # return render(request, 'display_img.html', {'imgs' : imgs})
         last_img = ImageModel.objects.order_by("id").last() 
         #img_gray = cv2.cvtColor(last_img, cv2.COLOR_BGR2GRAY)'
-        gray(last_img.img.url)
+        bb, contour4mask, df_n, ARR = infer_arr(str(settings.BASE_DIR) + last_img.img.url)
+        # gray(last_img.img.url)
+        result(last_img.img.url)
         last_img.gray = "gray.jpg"
         last_img.save()
+        print(last_img.img.url)
         return render(request, 'display_image.html', {'last_img' : last_img})
 
 
@@ -50,4 +58,14 @@ def gray(url):
     output = str(settings.BASE_DIR) + "/media/gray.jpg"
 
     cv2.imwrite(output, img_gray)
+
+def result(url):
+    print(1, url)
+    path = str(settings.BASE_DIR) + '/data/img_lr.png'
+    print(2, path)
+    img = cv2.imread(path)
+    result_img = img
+    output = str(settings.BASE_DIR) + "/media/gray.jpg"
+
+    cv2.imwrite(output, result_img)
    
